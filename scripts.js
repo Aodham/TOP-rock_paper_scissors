ff5d30ff44d0e5cbfe7b47a45a339ab209b71d6e
Aodham <aodham@protonmail.com>
2020-01-06 21:15:58 +0100

    Adds game loop for 5 rounds

--- scripts.js
+++ scripts.js
@@ -1,6 +1,8 @@
 let playerScore = 0;
 let cpuScore = 0;
-let round = 1;
+let round = 0;
+
+let startGame = "Y";
 
 function cpuPlay () {
     random = Math.random();
@@ -35,4 +37,27 @@ function playRound () {
         return roundMessage + "You loose this round.";
     }
     round ++;
+}
+
+function game () {
+    for (round = 0; round < 5; round ++) {
+        console.log(playRound());
+    }
+
+    if (playerScore > cpuScore) {
+            console.log("Great! You win this game.");
+    } else {
+            console.log("Ouch! You loose this game.");
+    }
+
+    console.log(`You have won ${playerScore} rounds. CPU has won ${cpuScore} rounds.`)
+
+    playerScore = 0;
+    cpuScore = 0;
+
+    startGame = prompt("Do you want to start another game?\nY to yes, N to no.")
+}
+
+while (startGame.charAt(0).toUpperCase() === "Y") {
+    game();
 }
