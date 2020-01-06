d29aba6eb6f428075e40f60d5ea6a446dd1e504a
Aodham <aodham@protonmail.com>
2020-01-06 19:34:16 +0100

    Adds capitalize function

--- scripts.js
+++ scripts.js
@@ -9,19 +9,11 @@ function computerPlay () {
     };
 }
 
-function playRound () {
-    let playerSelection = prompt("Choose your weapon: Rock, Paper or Scissors?");
-    let computerSelection = computerPlay();
-
-    if (playerSelection === computerSelection) {
-        console.log(`You choose the same that CPU. It's a tie!`);
-    } else if ((playerSelection === "Rock" && computerSelection === "Scissors") || (playerSelection === "Paper" && computerSelection === "Rock") || (playerSelection === "Scissors" && computerSelection === "Paper")) {
-        console.log(`Great! You choose ${playerSelection} and CPU chooses ${computerSelection}.`);
-    } else if ((playerSelection === "Rock" && computerSelection === "Paper") || (playerSelection === "Paper" && computerSelection === "Scissors") || (playerSelection === "Scissors" && computerSelection === "Rock")) {
-        console.log(`Ouch! You choose ${playerSelection} and CPU chooses ${computerSelection}.`);
-    } else {
-        console.log("Sorry, you entered an invalid weapon. Please write 'Rock', 'Paper' or 'Scissors'.")
-    }
+function capitalize (text) {
+    return text.charAt(0).toUpperCase() + text.slice(1);
 }
 
-const playerName = prompt("Please, enter your name.");
+function playRound () {
+    playerWeapon = capitalize(prompt("Please, choose your weapon.\nRock, Paper or Scissors?"))
+    console.log(playerWeapon);
+}
