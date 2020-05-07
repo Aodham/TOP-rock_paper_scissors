e9c626cf0d6d964ef7924dfb0083b2c671a959df
Aodham <aodhamm@gmail.com>
2020-05-07 04:56:49 +0200

    Adds all new divs to the HTML

--- scripts.js
+++ scripts.js
@@ -1,59 +1,43 @@
 let playerScore = 0;
 let cpuScore = 0;
-let round = 0;
+let round = 1;
 
-let startGame = "Y";
+let startGame = true;
+
+const container = document.querySelector("#container");
+const content = document.createElement("p");
 
 function cpuPlay () {
     const weapons = ["Rock", "Paper", "Scissors"];
     return weapons[Math.floor(Math.random()*weapons.length)];
 }
 
-function capitalize (text) {
+let capitalize = (text) =>{
     return text.charAt(0).toUpperCase() + text.slice(1);
 }
 
-function playRound () {
-    let playerWeapon = capitalize(prompt("Please, choose your weapon.\nRock, Paper or Scissors?"));
-    let cpuWeapon = cpuPlay();
-
-    let roundMessage = `You choose ${playerWeapon}, CPU chooses ${cpuWeapon}. `;
-
-    if (playerWeapon == cpuWeapon) {
-        return roundMessage + "It's a tie";
-    } else if ((playerWeapon == "Rock" && cpuWeapon == "Scissors")
-    || (playerWeapon == "Paper" && cpuWeapon == "Rock")
-    || (playerWeapon == "Scissors" && cpuWeapon == "Paper")) {
-        playerScore ++;
-        return roundMessage + "You win this round.";
-    } else {
-        cpuScore ++;
-        return roundMessage + "You loose this round.";
-    }
-    round ++;
-}
-
-function game () {
-    for (round = 0; round < 5; round ++) {
-        console.log(playRound());
-    }
-
-    if (playerScore > cpuScore) {
-            console.log("Great! You win this game.");
-    } else if (playerScore === cpuScore) {
-            console.log("Mmm, you've tied against the CPU.")
-    } else {
-            console.log("Ouch! You loose this game.");
-    }
-
-    console.log(`You have won ${playerScore} rounds. CPU has won ${cpuScore} rounds.`)
-
-    playerScore = 0;
-    cpuScore = 0;
-
-    startGame = prompt("Do you want to start another game?\nY to yes, N to no.")
-}
-
-while (startGame.charAt(0).toUpperCase() === "Y") {
-    game();
-}
+const buttons = document.querySelectorAll('button');
+buttons.forEach((button) => {
+    button.addEventListener('click', (e) => {
+        if (round < 5){
+            let playerWeapon = capitalize(button.id);
+            let cpuWeapon = cpuPlay();
+            let message = `Round ${round} of 5.<br>You choose ${playerWeapon}, CPU chooses ${cpuWeapon}.<br>Player Score: ${playerScore}<br>CPU Score: ${cpuScore}`;
+            if (playerWeapon == cpuWeapon) {
+                content.innerHTML = message + "<br>It's a tie";
+            } else if ((playerWeapon == "Rock" && cpuWeapon == "Scissors")
+            || (playerWeapon == "Paper" && cpuWeapon == "Rock")
+            || (playerWeapon == "Scissors" && cpuWeapon == "Paper")) {
+                playerScore ++;
+                content.innerHTML = message + "<br>You win this round.";
+            } else {
+                cpuScore ++;
+                content.innerHTML = message + "<br>You loose this round.";
+            }
+            round ++;
+        } else {
+            
+        }
+    })
+});
+container.appendChild(content);
