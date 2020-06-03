8659c0d8084433b5405eadbd1a3f86523293d7f4
Aodham <aodhamm@gmail.com>
2020-06-04 00:24:46 +0200

    Minimal Rock, paper, scissors code

--- scripts.js
+++ scripts.js
@@ -1,43 +1,57 @@
+// Constants
+
+const infoPanel = document.querySelector('#info');
+const buttons = document.querySelectorAll('button');
+const arenaButtons = document.querySelectorAll('#arena button');
+const reset = document.querySelector('#reset');
+
+let playerWeaon;
 let playerScore = 0;
+let cpuWeapon;
 let cpuScore = 0;
 let round = 1;
 
-let startGame = true;
 
-const container = document.querySelector("#container");
-const content = document.createElement("p");
+// Functions
 
-function cpuPlay () {
-    const weapons = ["Rock", "Paper", "Scissors"];
-    return weapons[Math.floor(Math.random()*weapons.length)];
+let capitalize = (text) => {
+	return text.charAt(0).toUpperCase() + text.slice(1);
 }
 
-let capitalize = (text) =>{
-    return text.charAt(0).toUpperCase() + text.slice(1);
+let cpuPlay = () => {
+	const weapons = ["Rock", "Paper", "Scissors"];
+	return weapons[Math.floor(Math.random()*weapons.length)];
 }
 
-const buttons = document.querySelectorAll('button');
-buttons.forEach((button) => {
-    button.addEventListener('click', (e) => {
-        if (round < 5){
-            let playerWeapon = capitalize(button.id);
-            let cpuWeapon = cpuPlay();
-            let message = `Round ${round} of 5.<br>You choose ${playerWeapon}, CPU chooses ${cpuWeapon}.<br>Player Score: ${playerScore}<br>CPU Score: ${cpuScore}`;
-            if (playerWeapon == cpuWeapon) {
-                content.innerHTML = message + "<br>It's a tie";
-            } else if ((playerWeapon == "Rock" && cpuWeapon == "Scissors")
-            || (playerWeapon == "Paper" && cpuWeapon == "Rock")
-            || (playerWeapon == "Scissors" && cpuWeapon == "Paper")) {
-                playerScore ++;
-                content.innerHTML = message + "<br>You win this round.";
-            } else {
-                cpuScore ++;
-                content.innerHTML = message + "<br>You loose this round.";
-            }
-            round ++;
-        } else {
-            
-        }
-    })
-});
-container.appendChild(content);
+let restart = () => {
+	playerScore = 0;
+	cpuScore = 0;
+	round = 1;
+	infoPanel.innerHTML = "^^ Choose your weapon ^^";
+	reset.style.display = "none";
+}
+
+arenaButtons.forEach((button) => {
+	button.addEventListener('click', (e) => {
+		reset.style.display = "block";
+		playerWeapon = capitalize(button.id);
+		cpuWeapon = cpuPlay();
+		let showInfo = (Message) => {
+			infoPanel.innerHTML = `Round ${round}.<br>---<br>You choose ${playerWeapon}, CPU chooses ${cpuWeapon}.<br>${Message}.<br>---<br>Player Score: ${playerScore}<br>CPU Score: ${cpuScore}`
+		}
+		if (playerWeapon == cpuWeapon) {
+			showInfo("It's a tie");
+		} else if ((playerWeapon == "Rock" && cpuWeapon == "Scissors") || (playerWeapon == "Paper" && cpuWeapon == "Rock") || (playerWeapon == "Scissors" && cpuWeapon == "Paper")) {
+			playerScore ++;
+			showInfo("You win this round");
+		} else {
+			cpuScore ++;
+			showInfo("You loose this round");
+		}
+		round ++;
+	})
+})
+
+reset.addEventListener('click', (e) => {
+	restart();
+})
